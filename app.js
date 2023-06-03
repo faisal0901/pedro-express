762d9f1165573a852d5eaa013cc03f6aa1e17f56
Muchammad Faisal <exmuhammadfaisal0402@gmail.com>
2023-06-03 11:02:26 +0700

    update feature

--- app.js
+++ app.js
@@ -8,7 +8,32 @@ const {
   List,
   MessageMedia,
 } = require("whatsapp-web.js");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env["API_KEY"],
+});
 
+const generateText = async (prompt) => {
+  try {
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.9,
+      max_tokens: 2048,
+    });
+    const text = response?.data?.choices?.[0]?.text ?? "tidak tahu";
+
+    console.log(text);
+    return text;
+  } catch (error) {
+    console.log(error.message);
+    // tangani error dengan cara yang tepat
+    return error.response.statusText;
+  }
+};
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", {
     root: __dirname,
@@ -56,8 +81,40 @@ client.on("ready", () => {
   console.log("client ready 1 ");
 });
 client.on("message", async (message) => {
+  function writeUsedToFile(data) {
+    const jsonData = JSON.stringify(data);
+    fs.writeFile("used.json", jsonData, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Data saved to file!");
+      }
+    });
+  }
+  let used = readUsedFromFile();
+  function countUsedParticipant(participant) {
+    // check if participant is already saved in used object
+    if (!used.hasOwnProperty(participant)) {
+      used[participant] = 1;
+    } else {
+      used[participant]++;
+    }
+
+    // write updated used object to file
+    writeUsedToFile(used);
+  }
+  function readUsedFromFile() {
+    try {
+      const jsonData = fs.readFileSync("used.json", "utf-8");
+      return JSON.parse(jsonData);
+    } catch (err) {
+      console.log(err);
+      return {};
+    }
+  }
   const chat = await message.getChat();
   console.log(message);
+
   if (
     message.hasMedia &&
     message.type === "image" &&
@@ -69,6 +126,12 @@ client.on("message", async (message) => {
       stickerName: "isal",
       sendMediaAsSticker: true,
     });
+    countUsedParticipant(message.from);
+  } else if (message.body.startsWith(".ai")) {
+    const prompt = message.body.slice(4);
+    const text = await generateText(prompt);
+    await message.reply(text);
+    countUsedParticipant(message.from);
   }
 });
 
